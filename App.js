e4241886941c5c62c2ee854c7b871812b2d1f8ce
tonaak <toannguyenitit@gmail.com>
2021-10-04 19:51:42 +0700

    Add FlatList

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, ScrollView, View, FlatList } from 'react-native';
 import CategoryListItem from './components/CategoryListItem';
 import Skii from './assets/skii.png';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <CategoryListItem title="skiing tools" imgSrc={Skii}/>
-      <CategoryListItem title="smurf tools" imgSrc={Skii}/>
-      <CategoryListItem title="re tools" imgSrc={Skii}/>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { id: 1, title: 'Dụng cụ trượt tuyết', imgSrc: Skii},
+        { id: 2, title: 'Quần áo trượt tuyết', imgSrc: Skii},
+        { id: 3, title: 'Thuốc trượt tuyết', imgSrc: Skii}
+      ]
+    };
+  };
+
+  render() {
+    const { categories } = this.state;
+    return (
+        <View style={styles.container}>
+          <FlatList 
+            contentContainerStyle={{padding: 16}}
+            data={categories}
+            renderItem={({ item }) => <CategoryListItem title={item.title} imgSrc={item.imgSrc} />}
+            keyExtractor={item => `${item.id}`}
+          />
+        </View>
+      );
+  };
+  
 }
 
 const styles = StyleSheet.create({
@@ -19,8 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFF',
     alignItems: 'stretch',
-    justifyContent: 'center',
-    marginLeft: 16,
-    marginRight: 16
+    justifyContent: 'center'
   }
 });
